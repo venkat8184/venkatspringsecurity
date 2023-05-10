fe0ffcc925709e52f8b34bfa6a8d6855b8276f47
venkat8184 <venkatramana8184@gmail.com>
2023-05-10 12:01:45 +0530

    initial Commit

---
+++ src/main/java/com/example/springsecurityram/SpringsecurityramApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityram/SpringsecurityramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
